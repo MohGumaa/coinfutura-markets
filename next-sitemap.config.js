ae6b8c2bf75c713c056c44f42998ff8f9e9715c2
MohGuma <gomaam14@gamil.com>
2025-11-16 19:01:08 +0400

    update meta data

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,18 +1,21 @@
 /** @type {import('next-sitemap').IConfig} */
+const SITE = process.env.SITE_URL || "https://market.coinfutura.com";
+
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://market.coinfutura.com",
+  siteUrl: SITE,
   generateRobotsTxt: true,
   generateIndexSitemap: true,
   changefreq: "daily",
   priority: 0.8,
   sitemapSize: 7000,
-
+  exclude: ["/404", "/500"],
   robotsTxtOptions: {
-    policies: [
-      { userAgent: "*", allow: "/" },
-    ],
+    policies: [{ userAgent: "*", allow: "/" }],
+    // additionalSitemaps should be full absolute URLs if used
+    // but you usually don't need to re-add the main /sitemap.xml
     additionalSitemaps: [
-      `${process.env.SITE_URL}/sitemap.xml`,
+      // If you have other sitemaps hosted elsewhere, put full URLs here.
+      // `${SITE}/sitemap.xml`  <-- usually not necessary
     ],
   },
 };
